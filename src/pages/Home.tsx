169529cb19bef1781c08aeecfbdce30e2c68645a
evandromottaz <evandro.motta@cantacom.com.br>
2022-06-24 12:52:33 -0300

    home mobile criado

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -32,21 +32,21 @@ export function Home() {
 
     return (
         <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-            <div className="w-full max-w-[1100px] flex justify-between mt-20 mx-auto">
-                <div className="max-w-[640px] ">
+            <div className="w-full max-w-[1100px] flex justify-between mt-20 mx-auto flex-wrap mobile:justify-center">
+                <div className="max-w-[640px] mobile:flex mobile:items-center mobile:flex-col px-7">
                     <Logo />
 
-                    <h1 className="mt-8 text-[2.5rem] leading-tight">
-                        Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React JS</strong>
+                    <h1 className="mt-8 text-4xl leading-tight mobile:text-center mobile:text-3xl">
+                        Construa uma <strong className="text-blue-500 inline-block">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React JS</strong>
                     </h1>
 
-                    <p className="mt-4 text-gray-200 leading-relaxed">
+                    <p className="mt-4 text-gray-200 leading-relaxed mobile:text-center mobile:mb-8 mobile:text-sm">
                         Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
                     </p>
                 </div>
 
-                <div className="p-8 bg-gray-700 border border-gray-500 rounded">
-                    <strong className="text-2xl mb-6 block">
+                <div className="p-8 bg-gray-700 border border-gray-500 rounded mobile:w-full">
+                    <strong className="text-2xl mb-6 block mobile:text-lg">
                         Inscreva-se gratuitamente
                     </strong>
 
