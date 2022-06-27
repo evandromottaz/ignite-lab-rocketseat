8601a0bf805c4515df67e57a0fd84685a68d22d7
evandromottaz <evandro.motta@cantacom.com.br>
2022-06-27 08:08:18 -0300

    remove useless imports

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,8 +1,4 @@
-import { Hourglass, HourglassHigh, HourglassLow, HourglassMedium, Power } from "phosphor-react";
-import { useEffect, useState } from "react";
-
 export function Loading() {
-
     return (
         <div className="flex-1 flex items-center justify-center">
             <div className="border-4 border-green-500 animate-spin rounded-full w-48 h-48 border-l-0"></div>
