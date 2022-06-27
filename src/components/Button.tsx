57bf6219091cb742fdeb7df5a742dcfe50f57a8d
evandromottaz <evandro.motta@cantacom.com.br>
2022-06-27 07:01:58 -0300

    hover effects on button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,13 +11,13 @@ type ColorProps = {
 }
 
 const color: ColorProps = {
-    green: 'bg-green-500 hover:bg-green-700',
+    green: 'bg-green-500 border-green-500 hover:border-green-700 hover:bg-green-700',
     blue: 'text-blue-500 border-blue-500 hover:text-gray-900 hover:bg-blue-500'
 }
 
 export function Button({ type, href, children }: ButtonProps) {
     return (
-        <a href={href} className={`p-4 text-sm border flex items-center rounded font-bold uppercase gap-2 justify-center transition-colors ${color[type]}`}>
+        <a href={href} className={`p-4 text-sm border flex items-center rounded font-semibold uppercase gap-2 justify-center transition-colors ${color[type]}`}>
             {children}
         </a>
     )
